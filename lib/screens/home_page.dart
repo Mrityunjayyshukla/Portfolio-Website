24af29629a2e903b7ec8357f5d6dcc5624900ab9
Mrityunjayyshukla <shuklamrityunjay60@gmail.com>
2024-10-23 13:26:00 +0530

    added contact me button

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -23,103 +23,151 @@ class _HomePageState extends State<HomePage> {
     const Text("Projects"),
     const Text("Experience"),
   ];
-  
-  void _onItemTapped(int index){
+
+  void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
 
+  bool _fabHover = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(100),
-          child: Padding(
-            padding: const EdgeInsets.only(right: 36.0, left: 8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset(
-                  "assets/memoji.png",
-                  height: 100,
-                  width: 100,
-                ),
-                Row(
-                  children: [
-                    MouseRegion(
-                      onEnter: (event) {
-                        setState(() {
-                          _isHovered1 = true;
-                        });
-                      },
-                      onExit: (event) {
-                        setState(() {
-                          _isHovered1 = false;
-                        });
-                      },
-                      child: Text(
-                        "Resume",
-                        style: GoogleFonts.roboto(
-                          fontSize: 24,
-                          fontWeight: FontWeight.w500,
-                          color: Theme.of(context).colorScheme.secondary,
-                          decoration: _isHovered1
-                              ? TextDecoration.underline
-                              : TextDecoration.none,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 32),
-                    MouseRegion(
-                      onEnter: (event) {
-                        setState(() {
-                          _isHovered2 = true;
-                        });
-                      },
-                      onExit: (event) {
-                        setState(() {
-                          _isHovered2 = false;
-                        });
-                      },
-                      child: Text(
-                        "About Me",
-                        style: GoogleFonts.roboto(
-                          fontSize: 24,
-                          fontWeight: FontWeight.w500,
-                          color: Theme.of(context).colorScheme.secondary,
-                          decoration: _isHovered2
-                              ? TextDecoration.underline
-                              : TextDecoration.none,
-                        ),
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ),
-        ),
-
-        // Body
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(100),
+        child: Padding(
+          padding: const EdgeInsets.only(right: 36.0, left: 8),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const LeftPanel(),
-              _widgetOptions[_selectedIndex],
-              const SizedBox(width: 80),
+              Image.asset(
+                "assets/memoji.png",
+                height: 100,
+                width: 100,
+              ),
+              Row(
+                children: [
+                  MouseRegion(
+                    onEnter: (event) {
+                      setState(() {
+                        _isHovered1 = true;
+                      });
+                    },
+                    onExit: (event) {
+                      setState(() {
+                        _isHovered1 = false;
+                      });
+                    },
+                    child: Text(
+                      "Resume",
+                      style: GoogleFonts.roboto(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w500,
+                        color: Theme.of(context).colorScheme.secondary,
+                        decoration: _isHovered1
+                            ? TextDecoration.underline
+                            : TextDecoration.none,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 32),
+                  MouseRegion(
+                    onEnter: (event) {
+                      setState(() {
+                        _isHovered2 = true;
+                      });
+                    },
+                    onExit: (event) {
+                      setState(() {
+                        _isHovered2 = false;
+                      });
+                    },
+                    child: Text(
+                      "About Me",
+                      style: GoogleFonts.roboto(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w500,
+                        color: Theme.of(context).colorScheme.secondary,
+                        decoration: _isHovered2
+                            ? TextDecoration.underline
+                            : TextDecoration.none,
+                      ),
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
-        
-        bottomNavigationBar: BottomNavBar(
-          selectedIndex: _selectedIndex,
-          onItemTapped: _onItemTapped,
-        )
+      ),
+
+      // Body
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const LeftPanel(),
+            _widgetOptions[_selectedIndex],
+            const SizedBox(width: 80),
+          ],
+        ),
+      ),
+
+      // Bottom Nav Bar
+      bottomNavigationBar: BottomNavBar(
+        selectedIndex: _selectedIndex,
+        onItemTapped: _onItemTapped,
+      ),
+
+      // Floating Action Button
+      floatingActionButton: MouseRegion(
+        onEnter: (event) {
+          setState(() {
+            _fabHover = true;
+          });
+        },
+        onExit: (event) {
+          setState(() {
+            _fabHover = false;
+          });
+        },
+        child: GestureDetector(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+              decoration: BoxDecoration(
+                color: _fabHover? Theme.of(context).colorScheme.secondary : Theme.of(context).colorScheme.primary,
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 24,
+                    color: Theme.of(context).colorScheme.shadow,
+                    offset: const Offset(10, 10),
+                  ),
+                  const BoxShadow(
+                    blurRadius: 24,
+                    color: Color(0xFFFFFFFF),
+                    offset: Offset(-10, -10),
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(40),
+              ),
+              child: Text(
+                "Contact Me",
+                style: GoogleFonts.roboto(
+                  color: _fabHover ? Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.secondary,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }

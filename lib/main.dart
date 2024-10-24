1a57be21bfe155abf6261af56d076294bb21bb1c
Mrityunjayyshukla <shuklamrityunjay60@gmail.com>
2024-10-25 01:23:55 +0530

    removed ThumbScrollBehaviour

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio/screens/home_page.dart';
 import 'package:portfolio/utils/color_palette.dart';
@@ -6,12 +7,23 @@ void main() {
   runApp(const MyApp());
 }
 
+class NoThumbScrollBehavior extends ScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        PointerDeviceKind.stylus,
+        PointerDeviceKind.trackpad,
+      };
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      scrollBehavior: NoThumbScrollBehavior().copyWith(scrollbars: false),
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: lightMode,
@@ -20,3 +32,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+

40a845751c157643403023a44782df72e536eb53
Mrityunjayyshukla <155085552+Mrityunjayyshukla@users.noreply.github.com>
2024-12-03 19:18:32 +0000

    Deploying to gh-pages from @ Mrityunjayyshukla/Portfolio-Website@50443fe8230840af901cd03e355e410ddc9560cd 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3886214722"
+    serviceWorkerVersion: "2976014855"
   }
 });

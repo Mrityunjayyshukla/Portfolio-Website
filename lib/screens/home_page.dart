33f3f32effd2b8162999e30a7e7e42cedacc1f43
Mrityunjayyshukla <shuklamrityunjay60@gmail.com>
2024-12-04 00:45:55 +0530

    minor fixes #3

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: use_build_context_synchronously
-
 import 'package:emailjs/emailjs.dart' as emailjs;
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
